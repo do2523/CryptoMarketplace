646f0ad27249a84508fe0b20566abbc75806996b
luciano093 <luparedes100@gmail.com>
2025-03-28 14:36:34 -0400

    Display wallet address and balance on wallet page

--- backend/routes/bitcoin.ts
+++ backend/routes/bitcoin.ts
@@ -1,21 +1,31 @@
 import bitcoin from 'bitcoinjs-lib';
 import ECPairFactory from 'ecpair';
 import * as secp256k1 from 'tiny-secp256k1';
-import axios from 'axios';
 
 import express from 'express';
 
 const ECPair = ECPairFactory(secp256k1);
 
+type SingleAddress = {
+  "hash160": string,
+  "address": string,
+  "n_tx": number,
+  "n_unredeemed": number,
+  "total_received": number,
+  "total_sent": number,
+  "final_balance": number,
+  "txs": [string]
+}
+
 // Generate BTC Wallet
 export function createWallet(_req: express.Request, res: express.Response) {
   const keyPair = ECPair.makeRandom();
   const { address } = bitcoin.payments.p2pkh({ pubkey: Buffer.from(keyPair.publicKey) });
-  const privateKey = keyPair.toWIF();
+  const private_key = keyPair.toWIF();
 
   res.json({
     address,
-    privateKey,
+    private_key,
   });
 };
 
@@ -23,7 +33,22 @@ export function createWallet(_req: express.Request, res: express.Response) {
 export async function getBalance(req: express.Request, res: express.Response) {
   const address = req.query.address;
 
-  const url = `https://blockchain.info/q/addressbalance/${address}`;
-  const { data } = await axios.get(url);
-  res.json({ balance: data / 1e8 }); // Convert satoshis to BTC
+  const url = `https://blockchain.info/rawaddr/${address}`;
+  const data = (await fetch(url));
+
+  console.log(data);
+
+  const address_data = await data.json() as SingleAddress;
+
+  console.log(address_data.n_tx);
+  console.log(address_data.total_received);
+  console.log(address_data.total_sent);
+
+  if (data.status == 429) {
+    res.statusCode = 429;
+    return;
+  }
+  
+
+  res.json({ balance: address_data.final_balance / 1e8 }); // Convert satoshis to BTC
 };
--- frontend/src/wallet.tsx
+++ frontend/src/wallet.tsx
@@ -1,9 +1,62 @@
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from "react";
+
+type Wallet = {
+    address: string,
+    private_key: string,
+}
 
 export default function Wallet() {
+    const [wallet, setWallet] = useState({
+        address: "",
+        private_key: ""
+    });
+
+    const [balance, setBalance] = useState({
+        balance: 0,
+    });
+
+    useEffect(() => {
+        const getWallet = async (): Promise<Wallet> => {
+            
+            // GET request using fetch inside useEffect React hook
+            const response = await fetch('http://localhost:5000/api/wallet', {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "*",
+                }
+            });
+
+            return await response.json();
+        };
+
+        getWallet().then((result) => {
+            setWallet(result);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (wallet.address == "") {
+            return;
+        }
+        
+        const getBalance = async(): Promise<{balance: number}> => {
+            const response = await fetch(`http://localhost:5000/api/getBalance?address=${wallet.address}`);
+
+            return await response.json();
+        }
+
+        getBalance().then((result) => {
+            setBalance(result);
+        });
+    }, [wallet.address]);
+    
     return <>
-    <div>
-        <Button>test</Button>
-    </div>
+        <h1>Bitcoin</h1>
+        <br/>
+        <a>Wallet address: {wallet.address}</a>
+        <br/>
+        <a>Balance: {balance.balance}</a>
     </>
 }
