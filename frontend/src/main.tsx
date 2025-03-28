56e2f09295d8633f79697dd8e364c0cdc5424a2d
gitcammerism <camialarconfern@gmail.com>
2025-03-28 17:54:58 -0400

    added new navbar and footer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,19 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Navbar from './components/Navbar.tsx'
+import NavHeader from './components/nav-header.tsx'
+// import Navbar from './components/Navbar.tsx'
 import Carousel from './components/Carousel.tsx'
 import SectionTwo from './components/section-two.tsx'
 import { SplineSceneBasic } from './components/InteractiveUI.tsx'
 import { SectionThree } from './components/section-three.tsx'
+import { Demo } from './components/demo.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar />
+    <NavHeader />
+    {/* <Navbar /> */}
     <SplineSceneBasic />
     <SectionThree />
     <SectionTwo />
     <Carousel />
     <App />
+    <Demo />
   </StrictMode>,
 )
