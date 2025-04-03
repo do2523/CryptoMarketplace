2de1e57fc2617ac57f82513a74ff76eefd370579
gitcammerism <camialarconfern@gmail.com>
2025-04-03 19:22:37 -0400

    scaled down the images component homepage

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -2,25 +2,25 @@ export default function Carousel() {
     return (
         <>
         <div className="carousel w-full">
-  <div id="item1" className="carousel-item w-full">
+  <div id="item1" className="carousel-item w-3/4 md:w-1/2 scale-90">
     <img
       src="../images/updateme.jpg"
-      className="w-full height-200" />
+      className="w-full h-64 object-cover rounded-lg" />
   </div>
   <div id="item2" className="carousel-item w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      className="w-full" />
+      className="w-full h-64 object-cover rounded-lg" />
   </div>
   <div id="item3" className="carousel-item w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-      className="w-full" />
+      className="w-full h-64 object-cover rounded-lg" />
   </div>
   <div id="item4" className="carousel-item w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-      className="w-full" />
+      className="w-full h-64 object-cover rounded-lg" />
   </div>
 </div>
 <div className="flex w-full justify-center gap-2 py-2">
