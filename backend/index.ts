3a5ff75acfba92e28db66e3a3744b23d61393b12
FernandoAilon <fernandoailon26@yahoo.com>
2025-04-11 18:01:05 -0400

    i have no idea what this was

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import { createWallet, getBalance } from "./routes/bitcoin.ts"
+import coinRoutes from './routes/coinRoutes.ts'
+import 'dotenv/config';
 
 const app = express();
-
 app.use(cors())
+
 app.use(express.json())
 
 app.post('/api/wallet', createWallet);
 app.get("/api/getBalance", getBalance);
+app.use('/api/coins', coinRoutes);
+app.use('/api/coins/Bitcoin', coinRoutes)
+
 
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port: ${5000}!`)
 })
+
